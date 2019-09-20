ad62590320d470c47bee9b2ebdb1ebeb8d19ce4c
saramichelee <saramflores8@gmail.com>
2019-09-20 14:35:51 -0500

    re-arranged folders

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,29 @@
+// from data.js
+var tableData = data;
+
+// select the button
+var button = d3.select("#filter-btn");
+
+
+button.on("click", function(){
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(dateSearch => dateSearch.datetime === inputValue);
+
+    console.log(filteredData);
+
+    var tbody = d3.select("tbody");
+
+    filteredData.forEach(r => {
+        var row = tbody.append("tr");
+        Object.entried(r).forEach(([key,value]) => {
+            var cell = row.append("td");
+            cell.tyext(value);
+        });
+    });
+});
